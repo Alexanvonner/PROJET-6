bdf6fd395cb251228b6498e7899ac347674cddc5
Alexan <alexan.vonner73@gmail.com>
2021-12-04 15:22:46 +0100

    ajout bcrypt & crypto-js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,20 +1,42 @@
-// importation models de la bdd User.js
+// importation de bcrypt
+const bcrypt = require("bcrypt");
 
+// importation de crypto-js
+const CryptoJS = require("crypto-js");
+
+
+// importation models de la bdd User.js
 const User = require("../models/User");
+
+
 
 exports.signup = (req, res, next) => {
 console.log("req.body");
 console.log(req.body);
-const user = new User({
-    email : req.body.email,
-    password : req.body.password
-});
-console.log("contenu : USER  controllers/user.js");
-console.log(user);
 
+const emailCryptoJs = CryptoJS.AES.encrypt(req.body.email,"PRIVATE_KEY").toString();
+
+
+// salt combien de fois sera executer l'algo de hashage
+bcrypt.hash(req.body.password , 10)
+.then((hash) => {
+    // ce qui sera enregistrer dans MONGODB
+    const user = new User({
+    email : emailCryptoJs,
+    password : hash
+});
 user.save()
 .then(()=> res.status(201).json({message : "user create"}))
 .catch((error )=> res.status(500).json({message: error}))
+
+
+
+
+
+})
+.catch((error)=> res.status(500).json({error}).send(console.log(error)));
+
+
 
 
 };
